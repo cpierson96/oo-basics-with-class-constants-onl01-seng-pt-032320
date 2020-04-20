7fb3765cda0978963fc8b2bda2ae79b946ffd90a
Claudia Pierson <c172@umbc.edu>
2020-04-20 00:43:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,28 @@
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#   BRANDS = [ ]
+#     def initialize(brand, shoe_condition="new")
+#       @brand = brand
+#       BRANDS << brand if not BRANDS.include?(brand)
+#     end
+#       def cobble
+#         self.condition = "new"
+#         puts "Your shoe is as good as new!"
+#
+#
+#   end
+# end
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  BRANDS = [ ]
-    def initialize(brand, shoe_condition="new")
-      @brand = brand
-      BRANDS << brand if not BRANDS.include?(brand)
-    end
-      def cobble
-        self.condition = "new"
-        puts "Your shoe is as good as new!"
-  
-    
-  end 
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
